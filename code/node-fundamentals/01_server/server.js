754530f4056c9d282baa6fd11c57c4725f9f087b
Joan Fernandez <jfernandezc98@gmail.com>
2020-10-01 12:53:13 -0500

    Servidor de Node.js, implementadas las Solicitudes y respuestas

--- code/node-fundamentals/01_server/server.js
+++ code/node-fundamentals/01_server/server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const response = require('./network/response')
 
 const router = express.Router()
 
@@ -10,14 +9,13 @@ app.use( bodyParser.urlencoded( {extended:false} ) )
 app.use( router )
 
 router.get('/carrera', function(req, res) {
-    response.success( req, res, 'Lista de Carreras de la UPS.', 200 )
+    res.send ( 'Lista de carreras de la UPS. ')
 })
 router.post('/carrera', function(req, res) {
-    if (req.query.error == 'ok') {
-        response.error( req, res, 'Error al ingresar la Carrera.', 500 )        
-    } else {
-        response.success( req, res, 'Ingreso de Carrera exitoso.', 201 )        
-    }
+   console.log( req.body )
+   console.log( req.body )
+
+    res.status(201).send( {tipo_error:0, mensaje_error:'', mensaje_exito: 'Añadido exitosamente.' })
 })
 
 app.use( '/', express.static('public') )
