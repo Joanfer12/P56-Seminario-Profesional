c868aada80b7c516c0c000e273bea6fd41eb9282
Joan Fernandez <jfernandezc98@gmail.com>
2020-10-01 15:48:50 -0500

    Crud en javaScrip sin persistencia

--- code/node-project/components/carrera/controller.js
+++ code/node-project/components/carrera/controller.js
@@ -1,5 +1,52 @@
 const storage = require('./storage')
 
+function addCarrera(nombre, descripcion) {
+    return new Promise((resolve, reject) => {
+        let carrera = {
+            nombre: nombre,
+            descripcion: descripcion,
+        }
+        storage.add( carrera )
+        resolve( carrera )
+    })
+}
+
+function getCarreras(){
+    return new Promise((resolve, reject) => {
+        resolve(storage.get() )
+    })
+}
+function updateCarrera( nombre, descripcion) {
+    return new Promise(  (resolve, reject) => {
+        let carrera = {
+            nombre: nombre,
+            descripcion: descripcion,
+        }
+        storage.update( carrera)
+        resolve( carrera )
+    })
+}
+
+function deleteCarrera(nombre) {
+    return new Promise((resolve, reject) => {
+        storage.delete( nombre )
+           resolve( nombre )
+    })
+}
+
+module.exports = {
+    addCarrera,
+    getCarreras,
+    updateCarrera,
+    deleteCarrera,
+}
+
+
+
+
+/*
+const storage = require('./storage')
+
 function addCarrera(nombre, abreviatura, descripcion) {
     return new Promise((resolve, reject) => {
         let carrera = {
@@ -44,3 +91,4 @@ module.exports = {
     updateCarrera,
     deleteCarrera,
 }
+*/
--- code/node-project/components/carrera/storage.js
+++ code/node-project/components/carrera/storage.js
@@ -1,4 +1,39 @@
-const model = require('./model')
+const list = []
+
+function addCarrera( objeto ){
+    list.push( objeto )
+}
+
+function getCarreras() {
+     return list 
+ }
+
+function updateCarrera(objejo){
+    for (let i=0; i<list.length; i++){
+        if(list[i].nombre == objeto.nombre){
+             list[i] = objeto
+        }
+    }
+}
+
+ function deleteCarrera(nombre){
+     for (let i=0; i<list.length; i++){
+         if(list[i].nombre == nombre){
+             delete list[i]
+         }
+     }
+ }
+ module.exports = {
+    add: addCarrera,
+    get: getCarreras,
+    update:  updateCarrera,
+    delete: deleteCarrera,
+}
+
+
+
+
+/*const model = require('./model')
 
 function addCarrera( objeto ) {
     const carrera = new model( objeto )
@@ -35,3 +70,4 @@ module.exports = {
     update: updateCarrera,
     delete: deleteCarrera,
 }
+*/
